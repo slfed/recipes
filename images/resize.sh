cf98fa848196da896c01005923bafea3c45049e3
Chris Arderne <chris@rdrn.me>
2020-07-20 10:01:37 +0200

    Auto-add images with correct filename

--- images/resize.sh
+++ images/resize.sh
@@ -3,6 +3,8 @@
 for f in originals/*.jpg; do
     name=$(echo $f | sed -r "s/.+\/(.+)\..+/\1/");
     out=$name.jpg
-    echo $name.jpg
-    convert $f -resize 600x600 -quality 70 $out
+    if [ ! -f $out ]; then
+        echo $out
+        convert $f -resize 600x600 -quality 70 $out
+    fi
 done
